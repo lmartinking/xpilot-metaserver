277463263acb8c3e1a494de0aea4a2e4bc4e64ca
Piotr Kamisiński <rotunda@ktd.krakow.pl>
2015-07-04 00:41:29 +0200

    Added some overriding methods to ServerId. Improved the client scripts.

--- server.py
+++ server.py
@@ -7,6 +7,15 @@ class ServerId:
 		self.ip_addr = server_ip
 		self.port = server_port
 
+	def __eq__(self, other):
+		return (self.ip_addr, self.port) == (other.ip_addr, other.port)
+
+	def __hash__(self):
+		return hash((self.ip_addr, self.port))
+
+	def __str__(self):
+		return "ip_addr=" + format(self.ip_addr) + " port=" + str(self.port)
+
 class ServerInfo:
 	def __init__(self, server_id, commands):
 		self.init_params()
--- tools/tcpclient.py
+++ tools/tcpclient.py
@@ -3,6 +3,7 @@
 import socket
 import sys
 
+#HOST, PORT = "meta2.xpilot.org", 4401
 HOST, PORT = "localhost", 4401
 data = " ".join(sys.argv[1:])
 
@@ -10,14 +11,16 @@ data = " ".join(sys.argv[1:])
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    #sock.sendall(data + "\n")
+	# Connect to server and send data
+	sock.connect((HOST, PORT))
+	#sock.sendall(data + "\n")
 
-    # Receive data from the server and shut down
-    received = sock.recv(1024)
+	data = sock.recv(1024)
+	string = ""
+	while len(data):
+		string = string + data
+		data = sock.recv(1024)
+	print string
+
 finally:
-    sock.close()
-
-#print "Sent:     {}".format(data)
-print "Received: {}".format(received)
+	sock.close()
