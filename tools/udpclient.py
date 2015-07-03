bffc73ee65f7f69ab0debcfd8f89a17eab5a92d0
Piotr Kamisiński <rotunda@ktd.krakow.pl>
2015-07-03 22:17:17 +0200

    Moved some test scripts into the project. Moved protocol description to a directory.

---
+++ tools/udpclient.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import socket
+import sys
+
+HOST, PORT = "localhost", 5500
+# add server
+data = "add server e7440.zynty\nadd users 0\nadd version 1.4.6fxi~7\nadd map Xpilot-Tournament Map (Blood's Music)\nadd sizeMap 100x100\nadd author \"Patrick Kenny - pkenny@eecs.umich.edu\"\nadd bases 8\nadd fps 50\nadd port 15345\nadd mode ok\nadd teams 2\nadd free 2=4,4=4\nadd timing 0\nadd stime 1435952538\nadd queue 0\nadd sound no\n\nadd status SERVER VERSION...: 1.4.6fxi~7\nSTATUS...........: ok\nMAX SPEED........: 50 fps\nWORLD (100x100)..: Xpilot-Tournament Map (Blood's Music)\nAUTHOR.....: \"Patrick Kenny - pkenny@eecs.umich.edu\"\nPLAYERS ( 0/ 8)..:\nadd players Test1=test@xpilot.org{2},Test2=test@xpilot.org{4}"
+# remove server
+#data = "server e7440.zynty\nremove"
+
+# SOCK_DGRAM is the socket type to use for UDP sockets
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# As you can see, there is no connect() call; UDP has no connections.
+# Instead, data is directly sent to the recipient via sendto().
+sock.sendto(data, (HOST, PORT))
+#received = sock.recv(1024)
+
+print "Sent:     {}".format(data)
+#print "Received: {}".format(received)
+
